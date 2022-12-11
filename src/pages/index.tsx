17b29f7c969f6839c6a758f84c1c7c85342792ed
Mirza Chilman <mirzachilman@gmail.com>
2022-12-11 19:10:54 +0700

    feat: remove team members section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import ContactForm from '@/components/ContactForm';
 import Hero from '@/components/Hero';
 import PracticeAreas from '@/components/PracticeAreas';
-import Teams from '@/components/Teams';
 
 const Index = () => {
   return (
     <>
       <Hero />
       <PracticeAreas />
-      <Teams />
       <ContactForm />
     </>
   );
