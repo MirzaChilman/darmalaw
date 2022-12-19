e6153cd4d80fa0081684affce838908fd6f9ceb9
Mirza Chilman <mirzachilman@gmail.com>
2022-12-19 14:46:06 +0700

    feat: fix prettier eslint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,12 @@ import HeaderBottom from './HeaderBottom';
 import HeaderTop from './HeaderTop';
 
 const Header = () => {
-                                                                                                                        return (
-                                                                                                                                                                                                                                                <div className="mx-auto max-w-7xl">
-                                                                                                                                                                                                                                                                                                                                                                        <HeaderTop />
-                                                                                                                                                                                                                                                                                                                                                                        <HeaderBottom />
-                                                                                                                                                                                                                                                </div>
-                                                                                                                        );
+  return (
+    <div className="mx-auto max-w-7xl">
+      <HeaderTop />
+      <HeaderBottom />
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,22 +4,18 @@ import { AppConfig } from '@/utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
-                                                                                                                        // eslint-disable-next-line class-methods-use-this
-                                                                                                                        render() {
-                                                                                                                                                                                                                                                return (
-                                                                                                                                                                                                                                                                                                                                                                        <Html
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                lang={
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        AppConfig.locale
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                        >
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <Head />
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <body className="bg-gray-100">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <Main />
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <NextScript />
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </body>
-                                                                                                                                                                                                                                                                                                                                                                        </Html>
-                                                                                                                                                                                                                                                );
-                                                                                                                        }
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <Html lang={AppConfig.locale}>
+        <Head />
+        <body className="bg-gray-100">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
